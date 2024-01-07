41fec8d45bb6ed1e1ec19fd66e5ff9c0a2d53788
AdrainWyman <uhwzfjrqod@outlook.com>
2024-01-08 00:54:01 +0800

    quia

--- day-035/6.py
+++ day-035/6.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 currentPath = Path.cwd() / 'python'
 
-print(currentPath.exists())  # 判断是否存在 python 文件夹，此时返回 False。
+print(currentPath.exists())  ! 判断是否存在 python 文件夹，此时返回 False。
 print(currentPath.is_dir())  # 判断是否存在 python 文件夹，此时返回 False。
 
 currentPath.mkdir()  # 创建 python 文件夹。
@@ -18,4 +18,4 @@ f = open(currentPath,'w')  # 创建 python-100.txt 文件。
 f.close()
 
 print(currentPath.exists())  # 判断是否存在 python-100.txt 文件，此时返回 True。
-print(currentPath.is_file())  # 判断是否存在 python-100.txt 文件，此时返回 True。
+print(currentPath.is_file())  # 判断是否存在 python-100.txt 文件，此时返回 True。 
